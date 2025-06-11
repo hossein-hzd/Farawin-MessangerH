8bee61ac8b27e4f9bdbdeba3c6f5cf2fc900d5a9
hossein <hossein.nafti1374@gmail>
2025-06-11 14:17:59 +0330

    ویرایش قسمت ثبت نام

--- app/models/model_register.php
+++ app/models/model_register.php
@@ -8,14 +8,17 @@ class model_register extends model
     }
     function insert_data($post)
     {
-        $sql = "SELECT * FROM users WHERE user=?";
+        $sql = "SELECT * FROM usersm WHERE user=?";
         $params = array($post['username']);
         $result = $this->doSelect($sql, $params);
 
         if (sizeof($result) == 0) {
             $g=0;
-            $sql = "INSERT INTO users (user,password,registerdate) VALUES (?,?,?)";
-            $params = array($post['username'], md5($post['password']), self::jalali_date("Y/m/d"));
+            $sql = "INSERT INTO usersm (user,password,registerdate) VALUES (?,?,?)";
+            $jalaliDate = self::jalali_date("Y/m/d");
+            $gregorianDate = self::jalali_to_miladi($jalaliDate, '/', '-');
+            $dateTime = $gregorianDate . " 00:00:00";
+            $params = array($post['username'], md5($post['password']), $dateTime);
             $this->doQuery($sql, $params);
             echo json_encode(
                 array(
--- core/config.php
+++ core/config.php
@@ -4,5 +4,5 @@ const USERNAME = "root";
 const PASSWORD = "";
 const DATABASE = "hosseindb";
 const SERVER_NAME = "localhost";
-const URL = "https://localhost/Farawin-Messanger-master/";
+const URL = "https://localhost/hmessanger/";
 ?>
